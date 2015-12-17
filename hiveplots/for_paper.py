682d48acc2962f14f71eec76a4be1d3a268d2be8
clbarnes <cbarnes@mrc-lmb.cam.ac.uk>
2015-12-17 11:31:36 -0500

    rename paper_whole ini

--- hiveplots/for_paper.py
+++ hiveplots/for_paper.py
@@ -14,20 +14,20 @@ data_path = join(data_root, weakness, source, 'complete.json')
 G = utl.json_deserialise(data_path)
 M = MultiplexConnectome(G, 'etype')
 
-print('generating parent plot')
 phys = M.compose('GapJunction', 'Synapse')
 for node, degree in phys.degree().items():
     phys.node[node]['log_degree'] = log(degree) if degree else 0
 
 for log_deg in [True, False]:
-    phys_H = HivePlot(phys, config_path='config/paper.ini', order_nodes_by='log_degree' if log_deg else 'degree')
+    print('generating parent plot')
+    phys_H = HivePlot(phys, config_path='config/paper_whole.ini', order_nodes_by='log_degree' if log_deg else 'degree')
     phys_H.draw()
 
     # edge_colours = dict(zip(['GapJunction', 'Synapse', 'Monoamine'], palettable.colorbrewer.qualitative.Set1_3.mpl_colors))
 
     print('generating actual plot')
     whole = M.compose('GapJunction', 'Synapse', 'Monoamine')
-    H = HivePlot(whole, parent_hiveplot=phys_H, config_path='config/paper.ini')
+    H = HivePlot(whole, parent_hiveplot=phys_H, config_path='config/paper_whole.ini')
     H.draw()
     H.save_plot('./img/gj-syn-ma_ac-str_sort-phys{}.pdf'.format('-log' if log_deg else ''))
 
