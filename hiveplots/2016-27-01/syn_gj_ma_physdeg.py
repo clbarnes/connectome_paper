3684552c1762430738b925e4217cadb100a0e8c4
clbarnes <cbarnes@mrc-lmb.cam.ac.uk>
2016-01-27 12:47:11 -0500

    (1) showing Syn, Gap, MA with axes ordered by physical degree

--- hiveplots/2016-27-01/syn_gj_ma_physdeg.py
+++ hiveplots/2016-27-01/syn_gj_ma_physdeg.py
@@ -6,6 +6,7 @@ from os.path import join
 import connectome_utils as utl
 from multiplex import MultiplexConnectome
 from hiveplotter import HivePlot
+from palettable import colorbrewer as cb
 
 data_root = '/home/cbarnes/work/code/connectome/construct2/combine/tgt_data'
 data_paths = dict()
@@ -17,8 +18,12 @@ G = utl.json_deserialise(data_path)
 
 M = MultiplexConnectome(G, 'etype')
 
+etypes = ['GapJunction', 'Synapse', 'Monoamine']
+
+col_dict = dict(zip(etypes, cb.qualitative.Set1_4.mpl_colors))
+
 whole = M.compose('GapJunction', 'Synapse', 'Monoamine')
-H = HivePlot(whole, config_path='config/syn_gj_ma_physdeg.ini')
+H = HivePlot(whole, edge_category_colours=col_dict, config_path='config/syn_gj_ma_physdeg.ini')
 H.draw()
 H.save_plot('./img/syn_gj_ma_physdeg.pdf'.format(source, weakness))
 
